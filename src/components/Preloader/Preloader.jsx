36076288e04b4f8776b255b6fe51e7bc3375ca4a
Corbin Wolf <corbinwolf300@gmail.com>
2025-09-24 23:49:30 -0700

    feat(Preloader.jsx): add markup and styling

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,7 +1,14 @@
 import "./Preloader.css";
 
 function Preloader() {
-  return <div></div>;
+  return (
+    <section className="preloader">
+      <div className="preloader__content">
+        <div className="preloader__throbber"></div>
+        <p className="preloader__desc">Searching for news...</p>
+      </div>
+    </section>
+  );
 }
 
 export default Preloader;
