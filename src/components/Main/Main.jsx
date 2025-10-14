611ffadc641850715f0b415eee03558b83dbe4a2
Corbin Wolf <corbinwolf300@gmail.com>
2025-10-14 05:13:36 -0700

    feat(Main): add markup and styling

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,7 +1,30 @@
+import NewsCard from "../NewsCard/NewsCard";
+import Preloader from "../Preloader/Preloader";
+import About from "../About/About";
+
 import "./Main.css";
 
 function Main() {
-  return <div></div>;
+  return (
+    <div className="main__background">
+      <div className="main">
+        <div className="main__content">
+          <h2 className="main__title">Search results</h2>
+          <div className="main__card-container">
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+            <NewsCard />
+          </div>
+          <button className="main__btn" type="button">
+            Show more
+          </button>
+        </div>
+      </div>
+      <Preloader />
+      <About />
+    </div>
+  );
 }
 
 export default Main;
