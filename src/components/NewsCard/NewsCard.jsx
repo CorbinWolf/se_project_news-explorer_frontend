bbf4e5a7b5108c688d9db5894c5dc8dd61eb9b57
Corbin Wolf <corbinwolf300@gmail.com>
2025-10-21 01:00:23 -0700

    feat(NewsCard): add markup and styling

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -2,13 +2,15 @@ import "./NewsCard.css";
 
 function NewsCard() {
   return (
-    <div className="card">
+    <article className="card">
       <div className="card__img">
         <button className="card__btn"></button>
         <p className="card__flag">Sign in to save articles</p>
       </div>
       <div className="card__content">
-        <p className="card__date">Aprimay 12, 5034</p>
+        <time className="card__date" dateTime="">
+          Aprimay 12, 5034
+        </time>
         <h3 className="card__title">
           Temp Card Temp Card Temp Card Temp Card Temp Card Temp Card
         </h3>
@@ -19,9 +21,9 @@ function NewsCard() {
           Bird. Bird. Bird. Bird. Bird. Bird. Bird. Bird. Bird. Bird. Bird.
           Bird. Bird.{" "}
         </p>
-        <p className="card__publisher">John Newspaper</p>
+        <cite className="card__publisher">John Newspaper</cite>
       </div>
-    </div>
+    </article>
   );
 }
 
